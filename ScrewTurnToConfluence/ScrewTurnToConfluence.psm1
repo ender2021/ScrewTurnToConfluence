ac566a0db4b9307c5adfe59503c4efff4472474b
ender2021 <meadmiracle@gmail.com>
2019-05-09 13:21:35 -0700

    Fixed global variable name bug

--- ScrewTurnToConfluence/ScrewTurnToConfluence.psm1
+++ ScrewTurnToConfluence/ScrewTurnToConfluence.psm1
@@ -8,7 +8,7 @@ if(@($publicFiles).Count -gt 0) { $publicFiles.FullName | ForEach-Object { . $_ 
 Export-ModuleMember -Function $publicFiles.BaseName
 
 if($null -eq $global:ScrewTurnToConfluence) {
-	$global:PowerConfluence = @{}
+	$global:ScrewTurnToConfluence = @{}
 }
 
 $onRemove = {
