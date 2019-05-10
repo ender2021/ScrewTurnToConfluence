350bde1e98e0bb6842e89720346bacef0e33b16f
ender2021 <meadmiracle@gmail.com>
2019-05-10 16:03:30 -0700

    Set up a way to apply multiple text transforms easily, added some new transforms

--- ScrewTurnToConfluence/Public/Format-Content.ps1
+++ ScrewTurnToConfluence/Public/Format-Content.ps1
@@ -14,11 +14,31 @@ function Format-Content {
 
 
     process {
-        
-        $ContentToConvert = ($ContentToConvert -replace [char]0x00a0,'-' | Invoke-ConfluenceConvertContentBody -FromFormat "wiki" -ToFormat "storage").Value
-        $ContentToConvert = $ContentToConvert -replace "\[image\|\|\{UP\(.*?\)\}(.*?\.[a-zA-Z]*)\]",'[[File=$1]]'
- 
-        $results += $ContentToConvert
+        $replacements = @(
+            @{
+                find = [char]0x00a0
+                replace = '-'
+            },
+            @{
+                find = "\[image\:{UP\(.*?\)\}(.*?\.[a-zA-Z]*)(\]|\|)((.*?)\])?"
+                replace = '[[File=$1]]'
+            },
+            @{
+                find = '“'
+                replace = '"'
+            },
+            @{
+                find = '”'
+                replace = '"'
+            },
+            @{
+                find = [char]0x00ae
+                replace = "(R)"
+            }
+        )
+        $toSend = $ContentToConvert
+        $replacements | ForEach-Object { $toSend = $toSend -replace $_.find,$_.replace }
+        $results += ($toSend | Invoke-ConfluenceConvertContentBody -FromFormat "wiki" -ToFormat "storage").Value
     }
     
     end {
