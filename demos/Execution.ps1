590e357e88bc9e22ce949219ad04514298c477b9
ender2021 <meadmiracle@gmail.com>
2019-05-12 16:22:00 -0700

    Updated master execution script to support links, transcludes, and snippets

--- demos/Execution.ps1
+++ demos/Execution.ps1
@@ -7,14 +7,17 @@ Import-Module SqlServer -Force
 . $PSScriptRoot\configs\Config.ps1
 
 #configure space information
-$spaceKey = $Config.spaceKey + "2"
-$spaceName = $Config.spaceName + " 2"
+$spaceKey = $Config.spaceKey #+ "2"
+$spaceName = $Config.spaceName #+ " 2"
 
 #open a session
 Open-ConfluenceSession $ConfluenceCredentials.UserName $ConfluenceCredentials.ApiToken $ConfluenceCredentials.HostName
 
 #Reset the space
 Reset-ConfluenceSpace $spaceKey $spaceName -Verbose
+
+#get snippets
+$snippets = Get-ScrewTurnSnippet $Config.dbconnection $Config.dbName
 
 #get category (aka label) mapping info from ScrewTurn
 $labels = Get-ScrewTurnCategoryBinding $Config.dbconnection $Config.dbName
@@ -23,23 +26,25 @@ $labels = Get-ScrewTurnCategoryBinding $Config.dbconnection $Config.dbName
 $attachments = Get-ChildItem $Config.attachments
 
 #get a list of all pages
-$allPages = Get-ScrewTurnPageContent $Config.dbconnection $Config.dbName -Exclude @("WikiMarkup-Reference")
+$allPages = Get-ScrewTurnPageContent $Config.dbconnection $Config.dbName -Exclude @("WikiMarkup-Reference") -ReplaceColon " -"
 
 #create a splattable object to pass conversion parameters
 $convertParams = @{
     SpaceKey = $spaceKey
+    AllPages = $allPages
+    Snippets = $snippets
     Labels = $labels
     Attachments = $attachments
     Verbose = $true
 }
 
 #create specific page as a test
-$testPage = $allPages | Where-Object {
-    @("Admissions-Architecture") -contains $_.Name
-} | New-ConfluencePage @convertParams
+# $testPage = $allPages | Where-Object {
+#     @("Data-Access-Layer-Best-Practices-Data-Access-Layer","Data-Access-Layer-Best-Practices-Interface") -contains $_.Name
+# } | New-ConfluencePage @convertParams
 
 #fill space with content
-#$pages = $allPages | New-ConfluencePage @convertParams
+$pages = $allPages | New-ConfluencePage @convertParams
 
 #close the session
 Close-ConfluenceSession
