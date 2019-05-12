602d32ce0c03fc23f0661fdd3524bb3797c3d435
ender2021 <meadmiracle@gmail.com>
2019-05-11 19:55:55 -0700

    Function to create a panel to prepend to converted pages

---
+++ ScrewTurnToConfluence/Public/Format-PageMetaPanel.ps1
@@ -0,0 +1,25 @@
+function Format-PageMetaPanel {
+    [CmdletBinding()]
+    param (
+        # An object with properties corresponding to ScrewTurn PageContent table columns
+        [Parameter(Mandatory,Position=0,ValueFromPipeline,ValueFromPipelineByPropertyName)]
+        [pscustomobject]
+        $PageContentRow
+    )
+    begin {
+        $results = @()
+    }
+    process {
+        $createBy = $PageContentRow.Create_By
+        $createDate = Get-Date $PageContentRow.Create_Date -Format f
+        $updateBy = $PageContentRow.Last_Update_By
+        $updateDate = Get-Date $PageContentRow.Last_Update_Date -Format f
+
+        $message = @("This page was originally created by *$createBy* on *$createDate*",
+                     "Prior to migration, it was last updated by *$updateBy* on *$updateDate*") -join "`r`n"
+        $results += "{note:title=Page Migrated From ScrewTurn!}$message{note}"
+    }
+    end {
+        $results
+    }
+}
