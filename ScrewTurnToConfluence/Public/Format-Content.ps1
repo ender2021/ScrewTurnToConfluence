3d5b06592ee577b962a6e0de9c84629d4c4a2c87
ender2021 <meadmiracle@gmail.com>
2019-05-11 23:52:43 -0700

    Major refactoring of Format-Content

--- ScrewTurnToConfluence/Public/Format-Content.ps1
+++ ScrewTurnToConfluence/Public/Format-Content.ps1
@@ -1,37 +1,31 @@
 function Format-Content {
     [CmdletBinding()]
     param (
-        # A sample parameter
-        [Parameter(Mandatory,Position=0,ValueFromPipeline,ValueFromPipelineByPropertyName)]
-        [Alias("Content")]
-        [AllowEmptyString()]
-        [string]
-        $ContentToConvert,
-
-        # The title of the content
-        [Parameter(Position=1,ValueFromPipelineByPropertyName)]
-        [string]
-        $Title="Unknown"
+        # An object with properties corresponding to ScrewTurn PageContent table columns
+        [Parameter(Mandatory,Position=0,ValueFromPipeline)]
+        [pscustomobject]
+        $PageContentRow
     )
     begin {
+        Write-Verbose "Beginning content formatting"
         $results = @()
         $replacements = @(
             @{
-                find = "\[image(.*?){UP\(.*?\)\}(.*?\.[a-zA-Z]*)\s*?(\]|\|)((.*?)\])?"
-                replace = '!$2!'
+                find = "\[image(.*?){UP\((.*?)\)\}(.*?\.[a-zA-Z]*)\s*?(\]|\|)((.*?)\])?"
+                replace = '!$2^$3!'
             },
             @{
-                find = "\[\^?{UP\(.*?\)\}(.*?)\]"
-                replace = ('[' + $Title + '^$1]')
+                find = "\[\^?{UP\((.*?)\)\}(.*?)\]"
+                replace = '[$1^$2]'
             },
-            # @{
-            #     find = '(?<!\{)\{(?!(\{|TOC))'
-            #     replace = '&#0123;'
-            # },
-            # @{
-            #     find = '(?<!(\}|TOC))\}(?!\})'
-            #     replace = '&#0125;'
-            # },
+            @{
+                find = "\[\^(.*?)\]"
+                replace = '[$1]'
+            },
+            @{
+                find = '\[(.*?)\|(.*?)\]'
+                replace = '[$2|$1]'
+            },
             @{
                 find = "@@(xml|js|csharp|sql)?(\r\n|\n)?((.|\n)*?)@@"
                 replace = ('{code:language=$1}' + "`r`n" + '$3{code}')
@@ -85,6 +79,14 @@ function Format-Content {
                 replace = ''
             },
             @{
+                find = '(?<=\r\n|\n|\#)\#(?!(\s|\#))'
+                replace = '# '
+            },
+            @{
+                find = '(?<=\r\n|\n|\*)\*(?!(\s|\*))'
+                replace = '* '
+            },
+            @{
                 find = "\'\'\'(.*?)\'\'\'"
                 replace = '*$1*'
             },
@@ -93,36 +95,69 @@ function Format-Content {
                 replace = ('{panel}$1{panel}')
             },
             @{
-                find = '\<div style\=\"clear\: both\;?\" \/\>'
+                find = '\<div style\=\"clear\:\s?both\;?\" \/\>'
                 replace = ''
             },
             @{
-                find = '=====(.*?)====='
-                replace = 'h4. $1'
+                find = '(\r\n|\n)?=====(.*?)====='
+                replace = "`r`n" + 'h4. $2'
             },
             @{
-                find = '====(.*?)===='
-                replace = 'h3. $1'
+                find = '(\r\n|\n)?====(.*?)===='
+                replace = "`r`n" + 'h3. $2'
             },
             @{
-                find = '===(.*?)==='
-                replace = 'h2. $1'
+                find = '(\r\n|\n)?===(.*?)==='
+                replace = "`r`n" + 'h2. $2'
             },
             @{
-                find = '==(.*?)=='
-                replace = 'h1. $1'
+                find = '(\r\n|\n)?==(.*?)=='
+                replace = "`r`n" + 'h1. $2'
+            },
+            @{
+                find = '\{TOC\}'
+                replace = '{panel:title=Table of Contents}{TOC}{panel}'
             }
         )
     }
     process {
-        $toSend = $ContentToConvert
+        $name = $PageContentRow.Name
+        $title = $PageContentRow.Title
+
+        Write-Verbose "Beginning formatting of: $title ($name)"
+        $toSend = $PageContentRow.Content
+        $attachNames = @()
         if ($toSend -ne "") {
+            Write-Verbose "Performing basic string replacements on content"
             $replacements | ForEach-Object { $toSend = $toSend -replace $_.find,$_.replace }
+
+            Write-Verbose "Adding conversion note panel"
+            $toSend = (Format-PageMetaPanel $PageContentRow) + $toSend
+
+            Write-Verbose "Performing table formatting"
             $toSend = Format-ScrewTurnTables $toSend
+
+            Write-Verbose "Performing attachment name substitution and dependency collection"
+            #eliminate page prefixes, noting them as dependencies
+            [regex]$prefixes = "(\!|\[)(.*?)\^"
+            $prefixes.Matches($toSend) | ForEach-Object {
+                #what is going to be replaced
+                $original = $_.Value
+                
+                #remember the name and prefix char
+                $preChar = $_.Groups[1].Value
+                $foundName = $_.Groups[2].Value
+                if ($attachNames -notcontains $foundName) { $attachNames += $foundName}
+
+                #perform the replacement with a simple non-regex swap
+                $toSend = $toSend.Replace($original,$preChar)
+            }
+
             do {
                 $moreMacros = $false
                 try {
-                    $results += ($toSend | Invoke-ConfluenceConvertContentBody -FromFormat "wiki" -ToFormat "storage").Value            
+                    Write-Verbose "Attempting conversion to storage format"
+                    $toSend = ($toSend | Invoke-ConfluenceConvertContentBody -FromFormat "wiki" -ToFormat "storage").Value
                 }
                 catch {
                     $message = ($_.ErrorDetails.Message | ConvertFrom-Json).Message
@@ -130,21 +165,31 @@ function Format-Content {
                     if ($message -match $errorMatch) {
                         $moreMacros = $true
 
-                        [regex]$regex = $errorMatch
-                        $match = $regex.Matches($message)
-                        $macroContents = [regex]::Escape($match[0].Groups[1].Value)
+                        [regex]$errors = $errorMatch
+                        $macroContents = [regex]::Escape($errors.Matches($message)[0].Groups[1].Value)
                         $toSend = $toSend -ireplace "\{($macroContents)(\:.*?)?\}",'&#0123;$1$2&#0125;'
+                        Write-Verbose "Conversion failed due to unexpected macro {$macroContents}.  Replacing braces with ALT codes and trying again."
                     } else {
-                        $_
+                        Write-Verbose "Unexpected Error!"
+                        throw
                     }
                 }
             } while ($moreMacros)
+            Write-Verbose "Storage format conversion succeeded"
         } else {
-            $results += $toSend
+            Write-Verbose "Page is blank"
+        }
+        Write-Verbose "Finished formatting of: $title ($name)"
+
+        $results += [pscustomobject]@{
+            PageContentRow = $PageContentRow
+            FormattedContent = $toSend
+            Dependencies = $attachNames
         }
     }
     
     end {
+        Write-Verbose "Finished content formatting"
         $results
     }
 }
